5e2b17ba25baf1e876e1a22ca419999f7cce440f
eric-chen <eric-pw@qq.com>
2019-08-10 10:36:05 +0800

    获取url中最后一个/与?之间的名字

--- kits.js
+++ kits.js
@@ -126,3 +126,37 @@ function total(){
   });
   return total;
 }
+
+
+
+/**
+ * 转换思路是：
+        url参数长成： id=10086&name=goudan&pwd=123
+        把url参数使用  &  割开，成为  [键=值,键=值...]
+        再把数组里面的每个 键=值 再割开 ， [键,值]
+ * */
+kits.getUrlParams = function () {
+  let arr = location.search.substring(1).split('&');
+  let prams = {};
+  arr.forEach(e=>{
+      let temp = e.split('=');
+      let key = temp[0];
+      let val = temp[1];
+      prams[key] = val;
+  })
+  return prams;
+}
+
+/**
+ * 获取url中最后一个/与?之间的名字
+ * */
+kits.getRouterName=function(str){
+  let index = str.indexOf('?') // 查询参数标记
+        let routerName = ''
+        if(index == -1){ // 说明没有参数
+            routerName = str.substring(str.lastIndexOf('/')+1)
+        }else{
+            routerName = str.substring(str.lastIndexOf('/')+1,str.indexOf('?'))
+        }
+        return routerName
+}
